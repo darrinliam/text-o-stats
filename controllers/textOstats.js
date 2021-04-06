a08cf0f3b911293de5a87f58cb2038abc99019fd
darrinliam <darrinduford@gmail.com>
2021-04-06 13:15:59 -0400

    Beautified code.

--- controllers/textOstats.js
+++ controllers/textOstats.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const mathjs = require('mathjs');
@@ -10,172 +9,170 @@ const MAX_DISP_WORDS = 3;
 const MAX_SKIP_OMIT_WORDS = 50
 
 /** 
-* Helper function for populating response object and logging errors to the console.
-*/
+ * Helper function for populating response object and logging errors to the console.
+ */
 const resStatus = (res, code, success, message, console_msg) => {
-	if (console_msg)
-	  console.log(console_msg);		
-    res.status(code).send({ success, message });
-	return;
+  if (console_msg)
+    console.log(console_msg);
+  res.status(code).send({
+    success,
+    message
+  });
+  return;
 }
 
 
 /** 
-* Get and validate request parameters.
-*
-*   @param {object}  pdata - the request object that contains the params.
-*   @param {res}
-*   @return {array}
-*
-*/
-const  getParams = (pdata) => {
+ * Get and validate request parameters.
+ *
+ *   @param {object}  pdata - the request object that contains the params.
+ *   @param {res}
+ *   @return {array}
+ *
+ */
+const getParams = (pdata) => {
   let err = null;
   let data = {};
-  
+
   data["text"] = pdata.text;
-  if (!(data["text"]))
-  {
-	err = "Parameter 'text' is required.";
-	return [null, err]; 
+  if (!(data["text"])) {
+    err = "Parameter 'text' is required.";
+    return [null, err];
   }
-  
-  data["extended"] = pdata.metrics;   // A noop for now. Will be utlized in future commits
-  if (data["extended"] && 
-      (typeof(data["extended"]) !== 'boolean'))
-  {
-	err = "Parameter 'extended' must be either true or false.";
-	return [null, err]; 
+
+  data["extended"] = pdata.metrics; // A noop for now. Will be utlized in future commits
+  if (data["extended"] &&
+    (typeof(data["extended"]) !== 'boolean')) {
+    err = "Parameter 'extended' must be either true or false.";
+    return [null, err];
   }
-  
+
   data["omitWords"] = pdata.omitWords;
-  if (data["omitWords"])
-  {
-	if (!Array.isArray(data["omitWords"]))  // to do: check for strings in array
-	{
-		err = "Parameter 'omitWords' must be an array of strings.";
-		return [null, err]; 
-	}
-  }
+  if (data["omitWords"]) {
+    if (!Array.isArray(data["omitWords"])) // to do: check for strings in array
+    {
+      err = "Parameter 'omitWords' must be an array of strings.";
+      return [null, err];
+    }
+  } 
   else
-	data["omitWords"] = omitWordsDefault;
-  
+    data["omitWords"] = omitWordsDefault;
+
   data["maxDispWords"] = pdata.maxDispWords;
-  if (data["maxDispWords"] && 
-      ((isNaN(data["maxDispWords"])) || 
-	    (data["maxDispWords"] < 1) || (data["maxDispWords"] > MAX_SKIP_OMIT_WORDS)))
-  {
-	err = "Parameter 'maxDispWords' must be a positive number between 1 and 50.";
-	return [null, err]; 
+  if (data["maxDispWords"] &&
+    ((isNaN(data["maxDispWords"])) ||
+      (data["maxDispWords"] < 1) || (data["maxDispWords"] > MAX_SKIP_OMIT_WORDS))) {
+    err = "Parameter 'maxDispWords' must be a positive number between 1 and 50.";
+    return [null, err];
   }
-  
+
   data["skipFirst"] = pdata.skipFirst;
-  if (data["skipFirst"] && 
-      ((isNaN(data["skipFirst"])) || 
-	    (data["skipFirst"] < 1) || (data["skipFirst"] > MAX_SKIP_OMIT_WORDS)))
-  {
-	err = "Parameter 'skipFirst' must be a positive number between 1 and 50.";
-	return [null, err]; 
+  if (data["skipFirst"] &&
+    ((isNaN(data["skipFirst"])) ||
+      (data["skipFirst"] < 1) || (data["skipFirst"] > MAX_SKIP_OMIT_WORDS))) {
+    err = "Parameter 'skipFirst' must be a positive number between 1 and 50.";
+    return [null, err];
   }
-  
+
   return [data, null]
 }
 
 
 
 const wordSort = (a, b) => {
-  return (a.length - b.length);	
+  return (a.length - b.length);
 }
 
 const wordSortDesc = (a, b) => {
-  return (b.length - a.length);	
+  return (b.length - a.length);
 }
 
 
 // 
 /** 
-* Find all words in a sentence. wordMap gets modified.
-*
-*   @param {string} sentence
-*   @param {object} wordMap
-*   @returns {string[]}
-*/
-const getWords = (text, wordMap = {} ) => {
-	  
-	let words = [];
-	
-	// unicode letter characters match as word characters
-	let wordRe = /[\w\p{L}]+([-'\u2019][\w\p{L}]+)*/ug;
-	
-	let ans;
-	while (ans = wordRe.exec(text)) {
-		if (ans[0]) {
-			let ansLower = ans[0].toLowerCase();
-			if (wordMap[ansLower] === undefined)
-				wordMap[ansLower] = 1;
-			else
-				wordMap[ansLower]++;
-			words.push(ansLower);
-		}
-	}
-	return words;
+ * Find all words in a sentence. wordMap gets modified.
+ *
+ *   @param {string} sentence
+ *   @param {object} wordMap
+ *   @returns {string[]}
+ */
+const getWords = (text, wordMap = {}) => {
+
+  let words = [];
+
+  // unicode letter characters match as word characters
+  let wordRe = /[\w\p{L}]+([-'\u2019][\w\p{L}]+)*/ug;
+
+  let ans;
+  while (ans = wordRe.exec(text)) {
+    if (ans[0]) {
+      let ansLower = ans[0].toLowerCase();
+      if (wordMap[ansLower] === undefined)
+        wordMap[ansLower] = 1;
+      else
+        wordMap[ansLower]++;
+      words.push(ansLower);
+    }
+  }
+  return words;
 }
 
 /** 
-* Get the most frequently used words in the text.
-*
-*   @param {object} inData
-*   @param {object} wordMap
-*   @param {number} maxlen
-*   @returns {string[]}
-*/
+ * Get the most frequently used words in the text.
+ *
+ *   @param {object} inData
+ *   @param {object} wordMap
+ *   @param {number} maxlen
+ *   @returns {string[]}
+ */
 const getMaxWordFreq = (inData, wordMap, maxlen) => {
-	let maxq = [];
-	
-	const mapArrSort = (a, b) => {
-		return (b[0] - a[0]);
-	}
-	
-	let wordMapArr = [];
-	
-	for (let word in wordMap) {
-		if (!inData["omitWords"].includes(word))
-			wordMapArr.push([wordMap[word], word ]);
-	}
+  let maxq = [];
 
-	wordMapArr.sort(mapArrSort);
-	
-	maxq = wordMapArr.slice(0, maxlen);
-	
-	return maxq;
+  const mapArrSort = (a, b) => {
+    return (b[0] - a[0]);
+  }
+
+  let wordMapArr = [];
+
+  for (let word in wordMap) {
+    if (!inData["omitWords"].includes(word))
+      wordMapArr.push([wordMap[word], word]);
+  }
+
+  wordMapArr.sort(mapArrSort);
+
+  maxq = wordMapArr.slice(0, maxlen);
+
+  return maxq;
 }
 
 
 /** 
-* Splits text into sentences.
-*
-*   @param {string} text
-*   @returns {string[]}
-*/
+ * Splits text into sentences.
+ *
+ *   @param {string} text
+ *   @returns {string[]}
+ */
 const getSentences = (text) => {
-	// at least one space is needed after the terminating punctuation for a sentence to be found.
-	let sentRe = /([^.?!]+)([.!?]["]?(\s+|\s*$)|(\s*$))/g; 
-	// note:  ."\s+ ? at end, if any, are not saved and not needed.
-	
-	// As a convenience for the user, if the last sentence does not end in a period / ! / ? , 
-	// it will still count as a sentence.
-		
-	// to do: skip \n\n w/o period (for example, a title line)
-	
-	let sent;
-	let sentences = [];
-	
-	while (sent = sentRe.exec(text)) {
-		sentences.push(sent[1]);
-	}
-	
-	// to do: Mr. Dr., i.e.,  etc
-	
-	return sentences;
+  // at least one space is needed after the terminating punctuation for a sentence to be found.
+  let sentRe = /([^.?!]+)([.!?]["]?(\s+|\s*$)|(\s*$))/g;
+  // note:  ."\s+ ? at end, if any, are not saved and not needed.
+
+  // As a convenience for the user, if the last sentence does not end in a period / ! / ? , 
+  // it will still count as a sentence.
+
+  // to do: skip \n\n w/o period (for example, a title line)
+
+  let sent;
+  let sentences = [];
+
+  while (sent = sentRe.exec(text)) {
+    sentences.push(sent[1]);
+  }
+
+  // to do: Mr. Dr., i.e.,  etc
+
+  return sentences;
 }
 
 /** 
@@ -187,169 +184,175 @@ const getSentences = (text) => {
 *   @returns {number}
 */
 const getSentenceData = (inData) => {
-	
-	let wordsPerSent = [];
-	let sentenceData = {};
 
-    if (inData.skipFirst)
-	{
-		// Skip inData.skipFirst lines. Most likely a title / author header.
-		let newln_count = inData.skipFirst;
-		while (newln_count) {
-		  let newlnOffs = inData.text.indexOf('\n');
-		  if (newlnOffs < 0)
-		    break;
-		  inData.text = inData.text.slice(newlnOffs + 1);
-		  newln_count--;
-		}
-	}
+  let wordsPerSent = [];
+  let sentenceData = {};
 
-    let sentences = getSentences(inData.text);
+  if (inData.skipFirst) {
+    // Skip inData.skipFirst lines. Most likely a title / author header.
+    let newln_count = inData.skipFirst;
+    while (newln_count) {
+      let newlnOffs = inData.text.indexOf('\n');
+      if (newlnOffs < 0)
+        break;
+      inData.text = inData.text.slice(newlnOffs + 1);
+      newln_count--;
+    }
+  }
 
-	for (let sent of sentences) {
-		let words = getWords(sent);
-		wordsPerSent.push(words.length);
-	}
-	
-	sentenceData.wordsPerSentUnsorted = [...wordsPerSent];
-	wordsPerSent.sort( (a,b) => a - b);
-	sentenceData.wordsPerSent = wordsPerSent;
-	
-	if (sentences.length)
-	  sentenceData.stdDev = mathjs.std(sentenceData.wordsPerSent);
-	else
-	  sentenceData.stdDev = 0;
-	
-	return [sentences, sentenceData];
+  let sentences = getSentences(inData.text);
+
+  for (let sent of sentences) {
+    let words = getWords(sent);
+    wordsPerSent.push(words.length);
+  }
+
+  sentenceData.wordsPerSentUnsorted = [...wordsPerSent];
+  wordsPerSent.sort((a, b) => a - b);
+  sentenceData.wordsPerSent = wordsPerSent;
+
+  if (sentences.length)
+    sentenceData.stdDev = mathjs.std(sentenceData.wordsPerSent);
+  else
+    sentenceData.stdDev = 0;
+
+  return [sentences, sentenceData];
 }
 
 
 /** 
-* Computes average word length, not counting words from the omit list.
-*
-*   @param {object} inData
-*   @param {string[]} words
-*   @returns {number}
-*/
+ * Computes average word length, not counting words from the omit list.
+ *
+ *   @param {object} inData
+ *   @param {string[]} words
+ *   @returns {number}
+ */
 const getAvgWordLength = (inData, words) => {
-	
-	let totalLetters = 0;
-	let totalWords = words.length;
-	
-	for (let word of words) {
-		if (!inData["omitWords"].includes(word))
-		   totalLetters += word.length;
-		else
-		  totalWords--;  // remove this word from count
-	}
-	
-	let avg = totalLetters / totalWords;
-	return avg.toPrecision(4);
+
+  let totalLetters = 0;
+  let totalWords = words.length;
+
+  for (let word of words) {
+    if (!inData["omitWords"].includes(word))
+      totalLetters += word.length;
+    else
+      totalWords--; // remove this word from count
+  }
+
+  let avg = totalLetters / totalWords;
+  return avg.toPrecision(4);
 }
 
 /** 
-* Calculates statistics about the text.
-*
-*   @param {object} inData
-*   @returns {object}
-*/
+ * Calculates statistics about the text.
+ *
+ *   @param {object} inData
+ *   @returns {object}
+ */
 const wordCalc = (inData) => {
-	
-	let [sentences, sentenceData] = getSentenceData(inData);
-    let sentenceCount = 0; 
-	
-    let allWords = [];
-	let allWordMap = {};
-	let words = [];
-	let avgConseqSentLenDif = 0.0;
-	
-	for (let i = 0; i < sentences.length; i++)
-	{
-		words = getWords(sentences[i], allWordMap);
-		allWords.push(...words);
-	}
 
-    let lenDif = 0;
-	let lenDifArr = [];
-    for (let i = 0; i < sentenceData.wordsPerSentUnsorted.length; i ++) {		
-	    if (i > 0)
-		{
-		  lenDif = Math.abs(sentenceData.wordsPerSentUnsorted[i-1] - sentenceData.wordsPerSentUnsorted[i]);
-		  lenDifArr.push(lenDif);
-		}
-	}
-	
-	if (lenDifArr.length)
-        avgConseqSentLenDif = (lenDifArr.reduce((a,b) => a + b, 0) / lenDifArr.length).toPrecision(4);
-	
-	let dispMax = inData.maxDispWords ? inData.maxDispWords : MAX_DISP_WORDS;	
-	
-	let maxWordFreq = getMaxWordFreq(inData, allWordMap, dispMax);
-	for (let i = 0; i < maxWordFreq.length; i++) {  // special check for 'I'
-		if (maxWordFreq[i][1] === 'i')
-		{
-		  maxWordFreq[i][1] = "I";
-			 break;
-		}
-	}
-	
-	let wordCount = allWords.length;
-	let longestWordLen = 0;
-	let allWordsSort = [];
-	let longestWords = [];
-	let avgWordsPerSentence = 0;
-	let sentenceLenStdDev = 0.0;
-	let avgWordLength = 0.0;
-	
-	if (wordCount)
-	{
-		allWordsSort = allWords.sort(wordSort);
-		longestWords = Array.from(new Set(allWordsSort.slice(-dispMax)));
-		longestWords.sort(wordSortDesc);
-		longestWordLen = longestWords[0].length;
-		avgWordsPerSentence = Math.round(wordCount / sentences.length);
-		sentenceLenStdDev = sentenceData.stdDev.toPrecision(4);
-		avgWordLength = getAvgWordLength(inData, allWords);
-	}
+  let [sentences, sentenceData] = getSentenceData(inData);
+  let sentenceCount = 0;
 
-	/* to do:
-	    # of hypnenated words
-		# of --
-		# of ? and !
-	*/
-		
-	sentenceCount = sentences.length;
-	
-	let success = true;
-	
-	let outData = { success, wordCount, sentenceCount, avgWordsPerSentence, sentenceLenStdDev, 
-	                longestWords, longestWordLen, maxWordFreq, avgWordLength, avgConseqSentLenDif };
-	return outData;
+  let allWords = [];
+  let allWordMap = {};
+  let words = [];
+  let avgConseqSentLenDif = 0.0;
+
+  for (let i = 0; i < sentences.length; i++) {
+    words = getWords(sentences[i], allWordMap);
+    allWords.push(...words);
+  }
+
+  let lenDif = 0;
+  let lenDifArr = [];
+  for (let i = 0; i < sentenceData.wordsPerSentUnsorted.length; i++) {
+    if (i > 0) {
+      lenDif = Math.abs(sentenceData.wordsPerSentUnsorted[i - 1] - sentenceData.wordsPerSentUnsorted[i]);
+      lenDifArr.push(lenDif);
+    }
+  }
+
+  if (lenDifArr.length)
+    avgConseqSentLenDif = (lenDifArr.reduce((a, b) => a + b, 0) / lenDifArr.length).toPrecision(4);
+
+  let dispMax = inData.maxDispWords ? inData.maxDispWords : MAX_DISP_WORDS;
+
+  let maxWordFreq = getMaxWordFreq(inData, allWordMap, dispMax);
+  for (let i = 0; i < maxWordFreq.length; i++) { // special check for 'I'
+    if (maxWordFreq[i][1] === 'i') {
+      maxWordFreq[i][1] = "I";
+      break;
+    }
+  }
+
+  let wordCount = allWords.length;
+  let longestWordLen = 0;
+  let allWordsSort = [];
+  let longestWords = [];
+  let avgWordsPerSentence = 0;
+  let sentenceLenStdDev = 0.0;
+  let avgWordLength = 0.0;
+
+  if (wordCount) {
+    allWordsSort = allWords.sort(wordSort);
+    longestWords = Array.from(new Set(allWordsSort.slice(-dispMax)));
+    longestWords.sort(wordSortDesc);
+    longestWordLen = longestWords[0].length;
+    avgWordsPerSentence = Math.round(wordCount / sentences.length);
+    sentenceLenStdDev = sentenceData.stdDev.toPrecision(4);
+    avgWordLength = getAvgWordLength(inData, allWords);
+  }
+
+  /* to do:
+      # of hypnenated words
+  	# of --
+  	# of ? and !
+  */
+
+  sentenceCount = sentences.length;
+
+  let success = true;
+
+  let outData = {
+    success,
+    wordCount,
+    sentenceCount,
+    avgWordsPerSentence,
+    sentenceLenStdDev,
+    longestWords,
+    longestWordLen,
+    maxWordFreq,
+    avgWordLength,
+    avgConseqSentLenDif
+  };
+  return outData;
 }
 
 
 /**
-*   Endpoint for text-o-stat, the textual statistic generator. If successful, 
-*   responds with a JSON object containing statistics about the text.
-*
-*	@params {object} req
-*	@params {object} res
-*
-*/
-function textOstats ( req, res) {
-	let outData = {};
-	
-	let [ inData, err ] = getParams(req.body);
-	if (!inData)
-		return (resStatus(res, 400, false, `Bad parameters: ${err}`, err));
-	  
-	outData = wordCalc(inData);
-	if (outData)
-		res.json(outData);  // success
-	else
-		return (resStatus(res, 400, false, `No data returned.`));
+ *   Endpoint for text-o-stat, the textual statistic generator. If successful, 
+ *   responds with a JSON object containing statistics about the text.
+ *
+ *	@params {object} req
+ *	@params {object} res
+ *
+ */
+function textOstats(req, res) {
+  let outData = {};
+
+  let [inData, err] = getParams(req.body);
+  if (!inData)
+    return (resStatus(res, 400, false, `Bad parameters: ${err}`, err));
+
+  outData = wordCalc(inData);
+  if (outData)
+    res.json(outData); // success
+  else
+    return (resStatus(res, 400, false, `No data returned.`));
 }
 
 
-module.exports = { textOstats
-                   };
+module.exports = {
+  textOstats
+};
--- server/texsrv.js
+++ server/texsrv.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
@@ -10,7 +9,7 @@ const getArgs = require('./getArgs.js');
 
 const app = express();
 
-const DEFAULT_PORT=3001;
+const DEFAULT_PORT = 3001;
 
 const usage = `Usage: 
   texsrv  --verbose 
@@ -18,7 +17,7 @@ const usage = `Usage:
 `;
 
 // middleware to examine req/res
-const reqLogger = function (req, res, next) {
+const reqLogger = function(req, res, next) {
   console.log(`reqLogger: method: ${req.method}.`);
   next();
 }
@@ -28,31 +27,38 @@ app.use(reqLogger);
 app.use(bodyParser.json());
 app.use((err, req, res, next) => {
   if (err) {
-    res.status(400).send({success: false, message: "Error parsing body as JSON."})
+    res.status(400).send({
+      success: false,
+      message: "Error parsing body as JSON."
+    })
   } else {
     next()
   }
 })
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.use(compression()); 
+app.use(compression());
 
 app.use(router);
 
 app.use(function(req, res) {
-    res.status(404).send({ success: false, message: "Not found."}); 
+  res.status(404).send({
+    success: false,
+    message: "Not found."
+  });
 });
 
-let args = getArgs(process.argv.slice(2), usage); 
+let args = getArgs(process.argv.slice(2), usage);
 
 let port = (args.port) ? args.port : DEFAULT_PORT;
 
 const srv = app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
-if (args.verbose)
-{
+if (args.verbose) {
   console.log("Running in verbose mode.");
   setInterval(() => srv.getConnections(
     (err, openConnections) => console.log(`${openConnections} connections currently open`)
@@ -62,41 +68,40 @@ if (args.verbose)
 console.log(`Running environment: ${process.env.NODE_ENV ? process.env.NODE_ENV : "not set"}`);
 
 // Close all connections on interrupt/kill
-process.on('SIGINT', shutDown);  // ctrl-c
+process.on('SIGINT', shutDown); // ctrl-c
 process.on('SIGTERM', shutDown); // kill signal
 
 
 let connectionList = [];
 
 srv.on('connection', (connection) => {
-	console.log("Adding connection.");
-    connectionList.push(connection);
-    connection.on('close', () => {
-		connectionList = connectionList.filter( (curConn) => { 
-		  return (curConn !== connection);
-		});
-		console.log("Closed connection.");
-	});
+  console.log("Adding connection.");
+  connectionList.push(connection);
+  connection.on('close', () => {
+    connectionList = connectionList.filter((curConn) => {
+      return (curConn !== connection);
+    });
+    console.log("Closed connection.");
+  });
 });
 
 function shutDown() {
-    console.log("Received SIGTERM / SIGINT. Shutting down...");
-    srv.close(() => {
-        console.log("Done.");
-        process.exit(0);
-    });
+  console.log("Received SIGTERM / SIGINT. Shutting down...");
+  srv.close(() => {
+    console.log("Done.");
+    process.exit(0);
+  });
 
-    setTimeout(() => {
-        console.error("Could not close connection(s). Shutting down now.");
-        process.exit(1);
-    }, 10000);
+  setTimeout(() => {
+    console.error("Could not close connection(s). Shutting down now.");
+    process.exit(1);
+  }, 10000);
 
-    connectionList.forEach( (curConn) => {
-	  curConn.end();
-	  console.log("Successfully closed connection during shutdown.");
-	});
-    setTimeout(() => connectionList.forEach( (curConn) => curConn.destroy()), 5000);
+  connectionList.forEach((curConn) => {
+    curConn.end();
+    console.log("Successfully closed connection during shutdown.");
+  });
+  setTimeout(() => connectionList.forEach((curConn) => curConn.destroy()), 5000);
 }
-
 
 module.exports = app;
